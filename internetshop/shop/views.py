d05693d7b09703b66cb684bb60478d8929bc3973
User <kseniathecactus@gmail.com>
2024-07-17 11:53:18 +0300

    починила стили

--- internetshop/shop/views.py
+++ internetshop/shop/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here
-
 def home(request):
-    return render(request, "index.html")
+    products = Product.objects.all()
+
+    return render(request, "index.html", {
+        'products': products
+    })
+def view_product(request):
+    return render(request, 'product.html')
